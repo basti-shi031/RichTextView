27edb410270828f62602dd135b8476f1fbc5ee6f
basti-shi031 <876572078@qq.com>
2016-01-11 10:19:13 +0800

    增加定义颜色和下划线方法

--- richtextviewlibrary/src/main/java/com/basti/richtextviewlibrary/TextObject.java
+++ richtextviewlibrary/src/main/java/com/basti/richtextviewlibrary/TextObject.java
@@ -6,11 +6,31 @@ package com.basti.richtextviewlibrary;
  */
 public class TextObject {
 
-    private int mStartIndex;
-    private int mEndIndex;
+    private int mStartIndex;//样式的开始字符
+    private int mEndIndex;//结束字符
     //private SpannableStringBuilder mSpannableStringBuilder;
-    private String content;
-    private int mOptionType;
+    private String content;//文本内容
+    private int mOptionType;//操作符
+    private int color;//字体颜色
+    private boolean isUnderline;//是否有下划线
+    //...其他需要的属性可继承TextObject自行添加
+
+
+    public boolean isUnderline() {
+        return isUnderline;
+    }
+
+    public void setIsUnderline(boolean isUnderline) {
+        this.isUnderline = isUnderline;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 
     public TextObject(String content) {
         this.content = content;
